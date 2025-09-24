a5fd183966c95777357fed523c526783f90f4f37
Calastrophe <source.fancy398@passinbox.com>
2025-09-24 01:28:57 -0500

    Game update (14107)

--- output/networksystem_dll.cs
+++ output/networksystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-09-22 07:52:41.303127900 UTC
+// 2025-09-24 06:15:44.468077300 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: networksystem.dll
