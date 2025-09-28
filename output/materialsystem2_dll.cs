bcd11562dc0d191ecbc26262ea036e262c6a9c32
Calastrophe <source.fancy398@passinbox.com>
2025-09-28 01:25:31 -0500

    Game update (14108)

--- output/materialsystem2_dll.cs
+++ output/materialsystem2_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-09-28 06:22:35.121768900 UTC
+// 2025-09-28 06:25:12.278309200 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: materialsystem2.dll
