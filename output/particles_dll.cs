6f7603b73cb9c7520034a76dba27054de3e91cf9
Calastrophe <source.fancy398@passinbox.com>
2025-10-22 20:25:42 -0500

    Game update (14115)

--- output/particles_dll.cs
+++ output/particles_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-10-16 05:43:35.292643700 UTC
+// 2025-10-23 01:24:28.388991 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: particles.dll
